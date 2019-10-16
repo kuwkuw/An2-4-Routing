6bb3d8ee37cf49e76e6080bdfd51efb1486d0336
Oleh Kushch <oleh.kushch@mediakind.com>
2019-10-16 15:27:33 +0300

    Task 03. Routes Config

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AboutComponent, HomeComponent, PathNotFoundComponent } from './layout';
+
+const routes: Routes = [
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'about',
+        component: AboutComponent
+    },
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    },
+    {
+        path: '*',
+        component: PathNotFoundComponent
+    }];
+@NgModule({
+    imports: [
+        RouterModule.forRoot(routes)
+    ],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
