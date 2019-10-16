69440325c05a90604694abc92921eb879c523c60
Oleh Kushch <oleh.kushch@mediakind.com>
2019-10-16 15:24:04 +0300

    Task 02. Components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LayoutModule } from './layout/layout.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    LayoutModule
   ],
   providers: [
   ],
