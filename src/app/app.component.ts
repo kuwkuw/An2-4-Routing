83c26f846511f68c1c6151b94ed06b04c6c0f5de
Oleh Kushch <oleh.kushch@mediakind.com>
2019-10-16 17:21:32 +0300

    Task 05. <router-outlet>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent { }
+export class AppComponent {
+
+  onActivate($event: any, routerOutlet: RouterOutlet): void {
+    console.log('Activated component:', $event, routerOutlet);
+  }
+
+  onDeactivate($event: any, routerOutlet: RouterOutlet): void {
+    console.log('Deactivate component:', $event, routerOutlet);
+  }
+}
